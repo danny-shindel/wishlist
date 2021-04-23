ddad07022e4714ba73fadf0b995fcb34d8a2bbce
dshindel <dannyshindel@gmail.com>
2021-04-23 11:14:55 -0700

    Complete Step 3.1

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name="index"),
+    path('', views.Index.as_view(), name="index"),
     # path('create/', views.TodoCreate.as_view(), name="create"),  
     # path('delete/<int:todo_id>/', views.delete, name="delete"),  
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, DeleteView
+from .models import Item
 
 # Create your views here.
+
+class Index(ListView):
+    model = Item
+    fields = '__all__'
