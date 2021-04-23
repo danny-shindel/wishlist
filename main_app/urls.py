8cc1eb8b7fa35fb145742f52af8d78543b58c534
dshindel <dannyshindel@gmail.com>
2021-04-23 11:30:55 -0700

    complete step 3.4

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name="index"),
-    # path('create/', views.TodoCreate.as_view(), name="create"),  
-    # path('delete/<int:todo_id>/', views.delete, name="delete"),  
+    path('add/', views.Add.as_view(), name="add"),  
+    path('delete/<int:wish_id>/', views.delete, name="delete"),  
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView
 from .models import Item
 
 # Create your views here.
@@ -7,3 +7,15 @@ from .models import Item
 class Index(ListView):
     model = Item
     fields = '__all__'
+
+class Add(CreateView):
+    model = Item
+    fields = '__all__'
+
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+def delete(request, wish_id):
+    Item.objects.get(id=wish_id).delete()
+    return redirect('index')
